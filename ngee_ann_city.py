b1db00b39b992f45b14f1b4e4bddad8876c48843
Wen Yu Tan <s10203894@connect.np.edu.sg>
2022-12-31 00:05:20 +0800

    Base code score placed

--- ngee_ann_city.py
+++ ngee_ann_city.py
@@ -5,6 +5,265 @@ from flask import Flask, render_template, request
 
 #Funtions for the games
 
+#Score function
+#Note: This is the base code that I placed
+#Just make sure all the different types of buildings are accounted for
+#in scoring and change the output below to return instead of print
+#So thet the returned value can be outputted to the front end
+#If u cannot finish by 1-2 am, I will resolve it by tmr when I wake up
+#Before the sprint review
+def score(grid, poolList):
+  scoreBCH = 0
+  scoreFAC = 0
+  scoreHSE = 0
+  scoreHWY = 0
+  scoreSHP = 0
+  scoreMON = 0
+  
+  for building in poolList:
+    if building[0] == 'HSE':
+      # HSE Scoring
+      print('HSE:', end = ' ')
+      firstrun = 0
+      scoreHSE = 0
+      
+      for row in range(len(grid)):
+        for col in range(len(grid[row])):
+          if grid[row][col] == 'HSE':
+            indiv_scoreHSE = 0
+            adjBuildings = []
+            if row != 0:
+              adjBuildings.append(grid[row-1][col])
+            if row != 3:
+              adjBuildings.append(grid[row+1][col])
+            if col != 0:
+              adjBuildings.append(grid[row][col-1])
+            if col != 3:
+              adjBuildings.append(grid[row][col+1])
+
+            if 'FAC' in adjBuildings:
+              scoreHSE = 1
+              if firstrun == 0:
+                print('1', end = ' ')
+                firstrun = 1
+              else:
+                print('+ 1', end = ' ')
+            else:
+              for building in adjBuildings:
+                if building == 'HSE' or building == 'SHP':
+                  indiv_scoreHSE += 1
+                elif building == 'BCH':
+                  indiv_scoreHSE += 2
+            
+              scoreHSE += indiv_scoreHSE
+              if firstrun == 0:
+                print('{}'.format(indiv_scoreHSE), end = ' ')
+                firstrun = 1
+              else:
+                print('+ {}'.format(indiv_scoreHSE), end = ' ')
+                  
+      if scoreHSE == 0:
+        print('0')
+      else:
+        print('= {}'.format(scoreHSE)) # show total
+    
+    elif building[0] == 'FAC':
+      # FAC Scoring
+      print('FAC:', end = ' ')
+      # check how many factories are there
+      countFAC = 0
+      scoreFAC = 0
+      for row in grid:
+        for col in row:
+          if col == 'FAC':
+            countFAC += 1
+      
+      if countFAC < 5:
+        firstrun = 0
+        for i in range(countFAC):
+          scoreFAC += countFAC
+          if firstrun == 0:
+            print('{}'.format(countFAC), end = ' ')
+            firstrun = 1
+          else:
+            print('+ {}'.format(countFAC), end = ' ')
+      
+      elif countFAC > 4:
+        firstrun = 0
+        for i in range(4):
+          scoreFAC += 4
+          if firstrun == 0:
+            print('{}'.format(4), end = ' ')
+            firstrun = 1
+          else:
+            print('+ {}'.format(4), end = ' ')
+        
+        remaining = countFAC - 4
+        for i in range(remaining):
+          print('+ {}'.format(1), end = ' ')
+          scoreFAC += 1
+
+      if scoreFAC == 0:
+        print('0')
+      else:
+        print('= {}'.format(scoreFAC)) # show total
+    
+    elif building[0] == 'SHP':
+      #SHP Scoring
+      print('SHP:', end = ' ')
+      scoreSHP = 0
+      firstrun = 0
+      for row in range(len(grid)):
+        for col in range(len(grid[row])):
+          
+          # for 1 SHP
+          if grid[row][col] == 'SHP':
+            unique_adjList = [] # adjacent list
+            indivScore = 0
+            check_adj = []
+
+            if row != 0:
+              check_adj.append(grid[row-1][col])
+            if row != 3:
+              check_adj.append(grid[row+1][col])
+            if col != 0:
+              check_adj.append(grid[row][col-1])
+            if col != 3:
+              check_adj.append(grid[row][col+1])
+
+            for building in check_adj:
+              if building not in unique_adjList:
+                unique_adjList.append(building)
+
+            indivScore = len(unique_adjList)
+            if firstrun == 0:
+              print('{}'.format(indivScore), end = ' ')
+              firstrun = 1
+            else:
+              print('+ {}'.format(indivScore), end = ' ')
+
+            scoreSHP += indivScore
+
+      if scoreSHP == 0:
+        print('0')
+      else:
+        print('= {}'.format(scoreSHP)) # show total
+    
+    elif building[0] == 'HWY':
+      #HWY Scoring
+      scoreHWYList = []
+      for row in grid:
+        countHWY = 0
+        for x in row:
+          if x == 'HWY':
+            countHWY += 1
+          else:
+            if countHWY != 0:
+              scoreHWYList.append(countHWY)
+            countHWY = 0
+        if countHWY != 0:
+          scoreHWYList.append(countHWY)
+      
+      firstrun = 0
+      scoreHWY = 0
+      print('HWY:', end = ' ')
+      for score in scoreHWYList:
+        scoreHWY += score ** 2
+        for i in range(score):
+          if firstrun == 0:
+            print('{}'.format(score), end = ' ')
+            firstrun = 1
+          else:
+            print('+ {}'.format(score), end = ' ')
+
+      if scoreHWY == 0:
+        print('0')
+      else:
+        print('= {}'.format(scoreHWY)) # show total
+
+    elif building[0] == 'BCH':
+      # BCH Scoring
+      print('BCH:', end = ' ')
+      scoreBCH = 0
+      firstrun = 0
+      for row in range(len(grid)):
+        for col in range(len(grid[row])):
+          # BCH scoring
+          if grid[row][col] == 'BCH':
+            if col == 0 or col == 3:
+              scoreBCH += 3
+              if firstrun == 0:
+                print('3', end = ' ')
+                firstrun = 1
+              else:
+                print('+ 3', end = ' ')
+            else:
+              scoreBCH += 1
+              if firstrun == 0:
+                print('1', end = ' ')
+                firstrun = 1
+              else:
+                print('+ 1', end = ' ')
+
+      if scoreBCH == 0:
+        print('0')
+      else:
+        print('= {}'.format(scoreBCH)) # show total
+  
+    elif building[0] == 'MON':
+      # MON Scoring
+      print('MON:', end = ' ')
+      scoreMON = 0
+      firstrun = 0
+      checkMONlist = []
+
+      checkMONlist.append(grid[0][0])
+      checkMONlist.append(grid[3][0])
+      checkMONlist.append(grid[0][3])
+      checkMONlist.append(grid[3][3])
+
+      if checkMONlist.count('MON') >= 3:
+        for row in range(len(grid)):
+          for col in range(len(grid[row])):
+            if grid[row][col] == 'MON':
+              scoreMON += 4
+              if firstrun == 0:
+                print('4', end = ' ')
+                firstrun = 1
+              else:
+                print('+ 4', end = ' ')
+      else:
+        for row in range(len(grid)):
+          for col in range(len(grid[row])):
+            if grid[row][col] == 'MON':
+              if row == 0 or row == 3 or col == 0 or col == 3:
+                scoreMON += 2
+                if firstrun == 0:
+                  print('2', end = ' ')
+                  firstrun = 1
+                else:
+                  print('+ 2', end = ' ')
+              else:
+                scoreMON += 1
+                if firstrun == 0:
+                  print('1', end = ' ')
+                  firstrun = 1
+                else:
+                  print('+ 1', end = ' ')
+
+      if scoreMON == 0:
+        print('0')
+      else:
+        print('= {}'.format(scoreMON)) # show total  
+
+
+
+  totalScore = scoreBCH + scoreFAC + scoreHSE + scoreHWY + scoreSHP + scoreMON
+  print('Total score:', totalScore)
+  print()
+
+  return totalScore
+
 #Functions(s) to build a building in game
 def build_building(grid, build_choice, turns, option):
    x_axis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 'x', 't', ]
