5ccfb8ff72e25628e361ac3e152dc07b0875fb95
Wen Yu Tan <s10203894@connect.np.edu.sg>
2023-01-04 17:35:52 +0800

    Database attribute change, Save, start game UI alt

--- schema.sql
+++ schema.sql
@@ -1,11 +1,15 @@
 DROP TABLE IF EXISTS saved_games;
 
-CREATE TABLE saved_games (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    password TEXT NOT NULL,
-    grid BLOB NOT NULL,
-    turns INTEGER NOT NULL,
-    coins INTEGER NOT NULL,
-    total_score INTEGER NOT NULL
-);
+-- CREATE TABLE saved_games (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     name TEXT NOT NULL,
+--     password TEXT NOT NULL,
+--     status INTEGER NOT NULL
+--     grid BLOB NOT NULL,
+--     turns INTEGER NOT NULL,
+--     coins INTEGER NOT NULL,
+--     total_score INTEGER NOT NULL
+-- );
+
+INSERT INTO saved_games(id, name, password, status, grid, turns, coins, total_score)
+VALUE();
