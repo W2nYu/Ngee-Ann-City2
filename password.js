8aa40aaadeaba77c5efd91fdb642a1f23802e90d
adam <s10219423@connect.np.edu.sg>
2023-01-05 13:15:27 +0800

    table and popup form for load game

---
+++ password.js
@@ -0,0 +1,10 @@
+function login(){
+    var question = prompt("Please enter the password");
+
+    if(question === each_saved_game[2]){
+        parBis();
+    }
+    function parBis(){
+        
+    }
+}
